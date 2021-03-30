bee2301bea63fb84606b092bb363378d4e3a4118
Sun M Kim <mini@Suns-Mac-mini.home>
2021-03-29 18:05:56 -0700

    adding table

---
+++ client/src/components/pages/students/StudentListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const StudentListItem = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+StudentListItem.propTypes = {
+  
+}
+
+export default StudentListItem;
